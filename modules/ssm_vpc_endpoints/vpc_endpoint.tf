06576c0834de76a3cac2394326b43885113635dd
Nate Marks <nmarks@imprivata.com>
2020-06-04 07:58:25 -0400

    Fxing the endpoint names

--- modules/ssm_vpc_endpoints/vpc_endpoint.tf
+++ modules/ssm_vpc_endpoints/vpc_endpoint.tf
@@ -5,21 +5,21 @@ data "aws_vpc" "selected" {
 
 resource "aws_vpc_endpoint" "ssm" {
   vpc_id       = data.aws_vpc.selected.id
-  service_name = "ssm.${var.aws_region}.amazonaws.com"
+  service_name = "com.amazonaws.${var.aws_region}.ssm"
 
   tags = var.tags
 }
 
 resource "aws_vpc_endpoint" "ssmmessages" {
   vpc_id       = data.aws_vpc.selected.id
-  service_name = "ssmmessages.${var.aws_region}.amazonaws.com"
+  service_name = "com.amazonaws.${var.aws_region}.ssmmessages"
 
   tags = var.tags
 }
 
 resource "aws_vpc_endpoint" "ec2messages" {
   vpc_id       = data.aws_vpc.selected.id
-  service_name = "ec2messages.${var.aws_region}.amazonaws.com"
+  service_name = "com.amazonaws.${var.aws_region}.ec2messages"
 
   tags = var.tags
 }
