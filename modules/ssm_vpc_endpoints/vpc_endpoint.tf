25a99933aab09c3226320c69bb5c3611d19fcf7a
Nate Marks <nmarks@imprivata.com>
2020-06-04 08:14:27 -0400

    Correcting endpoint type

--- modules/ssm_vpc_endpoints/vpc_endpoint.tf
+++ modules/ssm_vpc_endpoints/vpc_endpoint.tf
@@ -6,20 +6,20 @@ data "aws_vpc" "selected" {
 resource "aws_vpc_endpoint" "ssm" {
   vpc_id       = data.aws_vpc.selected.id
   service_name = "com.amazonaws.${var.aws_region}.ssm"
-
+  vpc_endpoint_type = "Interface"
   tags = var.tags
 }
 
 resource "aws_vpc_endpoint" "ssmmessages" {
   vpc_id       = data.aws_vpc.selected.id
   service_name = "com.amazonaws.${var.aws_region}.ssmmessages"
-
+  vpc_endpoint_type = "Interface"
   tags = var.tags
 }
 
 resource "aws_vpc_endpoint" "ec2messages" {
   vpc_id       = data.aws_vpc.selected.id
   service_name = "com.amazonaws.${var.aws_region}.ec2messages"
-
+  vpc_endpoint_type = "Interface"
   tags = var.tags
 }
